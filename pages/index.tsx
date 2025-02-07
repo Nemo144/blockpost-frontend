cbc974fcfc223c5696b3cdc775e997f726f48ced
Nnamdi Okeke <onnamdialex@gmail.com>
2025-02-07 16:09:19 +0100

    :zap: Update functions for the jsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,6 +36,70 @@ const Home: NextPage = () => {
   const [contractAddress, setContractAddress] = useState(
     'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM'
   );
+
+  //user input handler
+  const handleMessageChange = (e: { target: { value: SetStateAction<string> } }) => {
+    setPost(e.target.value);
+  };
+
+  //handle contract call to write-post
+  const handleOpenContractCall = async () => {
+    const functionArgs = [stringUtf8CV(post)];
+
+    const postConditions = [
+      makeStandardSTXPostCondition(stxAddress!, FungibleConditionCode.LessEqual, '1000000'),
+    ];
+
+    await openContractCall({
+      contractAddress: contractAddress,
+      contractName: 'blockpost',
+      functionName: 'write-post',
+      functionArgs,
+      postConditions,
+      attachment: 'This is an attachment',
+      onFinish: async data => {
+        console.log('finished contract call!', data);
+        setResponse(data);
+      },
+      onCancel: () => {
+        console.log('popup closed');
+      },
+    });
+  };
+
+  //handle contract call to get-post
+  const getPost = useCallback(async () => {
+    if (isSignedIn) {
+      //args for function being called
+      const functionArgs = [standardPrincipalCV(`${stxAddress}`)];
+
+      //network param for callReadOnly
+      const network = new StacksMocknet();
+
+      //read only function call
+      const result = await callReadOnlyFunction({
+        contractAddress: contractAddress,
+        contractName: 'blockpost',
+        functionName: 'get-post',
+        functionArgs,
+        network,
+      });
+      console.log('getting result', result);
+      if (result.value) {
+        setPostedMessage(result.value.data);
+      }
+    }
+  }, []);
+
+  //run get post on signin to get message
+  useEffect(() => {
+    console.log('In useEffect');
+    getPost();
+  }, [isSignedIn]);
+
+  //check the stacks API every 10 seconds looking for changes
+  useInterval(getPost, 10000);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
